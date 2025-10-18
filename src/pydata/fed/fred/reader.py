2200b27b8794674476a5edbac10a2120756630f1
Marc <marc.delabarrera@gmail.com>
2025-10-18 20:47:45 +0200

    add fred reader

---
+++ src/pydata/fed/fred/reader.py
@@ -0,0 +1,66 @@
+
+import requests
+import pandas as pd
+import numpy as np
+import time
+import datetime 
+
+API_URL = 'https://api.stlouisfed.org/fred'
+
+ListLike = list[str]|pd.Series|str
+
+
+def parameters_to_url(parameters:dict)->str: 
+        return '&'.join([f'{p}={v}' for p,v in parameters.items()])
+
+
+def _timestamp_to_str(date:pd.Timestamp)->str:
+        return date.strftime('%Y-%m-%d')
+
+def get_series(series_id: ListLike, api_key:str,
+               date_from:str, date_to:str = datetime.date.today()
+              )->pd.DataFrame:
+        '''
+        Parameters
+        series_id: list of series_id
+        Returns
+        -------
+                pd.DataFrame   
+                Dataframe with series_id and date as index.
+        '''
+        date_from = _timestamp_to_str(date_from) if isinstance(date_from, pd.Timestamp) else date_from
+        date_to = _timestamp_to_str(date_to) if isinstance(date_to, pd.Timestamp) else date_to
+        series_id = series_id.to_list() if isinstance(series_id, pd.Series) else series_id
+        series_id = [series_id] if isinstance(series_id, str) else series_id
+        return pd.concat([_get_series(i, observation_start = date_from, observation_end = date_to, api_key=api_key) for i in series_id])
+    
+def _get_series(series_id:str, observation_start:str, observation_end:str, api_key:str, cooldown:int=30)->pd.DataFrame:
+        parameters = {'series_id': series_id, 
+                        'api_key': api_key,
+                        'observation_start': observation_start,
+                        'observation_end': observation_end,
+                        'file_type':'json'}
+        parameters = parameters_to_url(parameters)
+        request = requests.get(f'{API_URL}/series/observations?{parameters}')
+        if request.status_code == 200:
+                request = request.json()
+        else:
+                raise ValueError(f'Error in request {request}')
+        
+        try:
+                obs = request['observations'] 
+        except KeyError:               
+                if  request['error_code']==429:
+                        print(f'Error 429: too many requests, waiting {cooldown} seconds')
+                        time.sleep(cooldown)
+                        return get_series(series_id, observation_start=observation_start, observation_end=observation_end)
+                else:
+                        raise ValueError(f'Error in request {requests}')
+                
+        out = pd.DataFrame([[iobs['date'],series_id, iobs['value']] for iobs in obs],
+                            columns=['date','series_id','value'])
+        out.value = pd.to_numeric(out.value, errors='coerce')
+        out.date = pd.to_datetime(out.date, format = '%Y-%m-%d')
+        out['src']='fred'
+        out = out.set_index(['series_id','date'])
+        return out
