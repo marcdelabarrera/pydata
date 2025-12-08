f0a755538c04864f2b7003a7816b2ed44ffde2f0
Marc de la Barrera <marc.delabarrera@gmail.com>
2025-12-08 13:51:34 +0100

    add zero cupon

--- src/pydata/papers/gurkaynak/main.py
+++ src/pydata/papers/gurkaynak/main.py
@@ -5,7 +5,11 @@ import numpy as np
 
 PATH = Path(__file__).parent
 
-def read_data(date_from:pd.Timestamp = None, date_to:pd.Timestamp = None) -> pd.DataFrame:
+
+def _zero_coupon_columns():
+    return [f"SVENY{t:02d}" for t in range(1, 31)]
+
+def read_data(date_from:pd.Timestamp = None, date_to:pd.Timestamp = None, columns = None) -> pd.DataFrame:
     df = pd.read_csv(PATH / "feds200628.csv", skiprows = 8)
     df = df.rename(columns={"Date":"date"})
     df["date"] = pd.to_datetime(df["date"])
@@ -14,6 +18,8 @@ def read_data(date_from:pd.Timestamp = None, date_to:pd.Timestamp = None) -> pd.
         df = df[date_from:]
     if date_to:
         df = df[:date_to]
+    if columns == "zero-cupon":
+        df = df[_zero_coupon_columns()]
     return df
 
 def yield_curve(date:pd.Timestamp, series="zero-coupon") -> pd.Series:
@@ -22,7 +28,7 @@ def yield_curve(date:pd.Timestamp, series="zero-coupon") -> pd.Series:
     date = date if date in df.index else df.index[np.argmin(np.abs(df.index - date))]
     
     if series == "zero-coupon":
-        yield_curve = df.loc[date, [col for col in df.columns if col.startswith("SVENY")]]
+        yield_curve = df.loc[date, _zero_coupon_columns()]
     else:
         raise ValueError(f"Unknown series: {series}")
     yield_curve = yield_curve.rename("yield").reset_index(drop=True)
